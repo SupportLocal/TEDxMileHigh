7b863caa625a9075ab1f2ed9e0bcdf1c20aa3f1f
Levi Cook <levicook@gmail.com>
2013-05-28 09:33:56 -0600

    log errors from form.Post handler

--- handlers/form/post.go
+++ handlers/form/post.go
@@ -1,27 +1,26 @@
 package form
 
 import (
+	"log"
 	"net/http"
 	"supportlocal/TEDxMileHigh/mongo"
 )
 
 func Post(w http.ResponseWriter, r *http.Request) {
 	var (
-		form = &form{}
+		form form
 		err  error
 	)
 
-	if err = readJson(r, form); err != nil {
-		// todo log it
+	if err = readJson(r, &form); err != nil {
+		log.Printf("readJson failed %q", err)
 	}
 
-	if form.valid() {
-		// save it
-
+	if form.valid() { // save it
 		inboundMessage := form.toInBoundMessage()
 		inboundMessageRepo := mongo.InboundMessageRepo()
 		if err = inboundMessageRepo.Save(&inboundMessage); err != nil {
-			// todo log it
+			log.Printf("inboundMessageRepo.Save failed %q", err)
 		}
 	}
 
