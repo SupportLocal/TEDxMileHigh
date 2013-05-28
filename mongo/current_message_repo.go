12fc175d4f4a4145002718f8fa0e1cbd524cb05c
Levi Cook <levicook@gmail.com>
2013-05-28 09:34:20 -0600

    Teach CurrentMessage some basic self defense.

--- mongo/current_message_repo.go
+++ mongo/current_message_repo.go
@@ -1,18 +1,49 @@
 package mongo
 
 import (
+	"fmt"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
+	"strings"
 )
 
 type CurrentMessage struct {
-	Id      bson.ObjectId `bson:"_id"     json:"id"`
-	Author  string        `bson:"author"  json:"author"`
-	Comment string        `bson:"comment" json:"comment"`
+	Id      bson.ObjectId     `bson:"_id" json:"id"`
+	Author  string            `bson:"a"   json:"author"`
+	Comment string            `bson:"c"   json:"comment"`
+	Errors  map[string]string `bson:"-"   json:"-"`
+}
+
+func (m *CurrentMessage) valid() bool {
+	m.Errors = make(map[string]string)
+
+	if id := m.Id; !id.Valid() {
+		m.Errors["id"] = "is invalid"
+	}
+
+	if author := strings.TrimSpace(m.Author); len(author) == 0 {
+		m.Errors["author"] = "is required"
+	}
+
+	if comment := strings.TrimSpace(m.Comment); len(comment) == 0 {
+		m.Errors["comment"] = "is required"
+	}
+
+	return len(m.Errors) == 0
 }
 
 type currentMessageRepo struct {
 	collection *mgo.Collection
+}
+
+func (r currentMessageRepo) Save(currentMessage *CurrentMessage) error {
+	if !currentMessage.valid() {
+		return fmt.Errorf("currentMessage is invalid %#v", currentMessage.Errors)
+	}
+
+	_, err := r.collection.UpsertId(currentMessage.Id, currentMessage)
+
+	return err
 }
 
 func (r currentMessageRepo) Tail(callback func(CurrentMessage)) error {
